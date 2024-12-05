99ddfe925afb8e30e309114e11a7d11676baefcd
mberdev <mathieu.ber.dev@gmail.com>
2024-12-05 20:19:46 +0100

    day06 - boilerplate

---
+++ day06/CMakeProject/CMakeProject.cpp
@@ -0,0 +1,57 @@
+﻿#include "CMakeProject.h"
+#include "infrastructure/file_reader.hpp"
+//#include "input_parser.hpp"
+
+#include <vector>
+#include <filesystem>
+#include <iostream>
+#include <algorithm>
+#include <regex>
+#include <cassert>
+#include <tuple>
+#include <string>
+
+using namespace std;
+
+class App {
+public:
+    void run() {
+        try {
+            string inputFilePath = (std::filesystem::current_path() / "input_data" / "test_input.txt").string();
+            std::vector<std::string> lines = FileReader::readAll(inputFilePath, false);
+            processLines(lines);
+            cout << "Done." << endl << endl;
+        }
+        catch (const std::exception& e) {
+            cerr << endl << "An error occurred: " << endl;
+            cerr << e.what() << std::endl;
+            cerr << "Done (with errors)." << endl << endl;
+        }
+    }
+
+private:
+    void processLines(std::vector<std::string>& lines) {
+        //auto reports = InputParser::parseLines(lines);
+
+        part1(lines);
+        //part2(lines);
+    }
+
+    void part1(std::vector<std::string>& lines)
+    {
+
+    }
+
+    void part2(std::vector<std::string>& lines) {
+
+    }
+
+};
+
+int main() {
+    App app;
+    app.run();
+    return 0;
+}
+
+
