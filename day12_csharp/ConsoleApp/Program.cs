22daf0720c024a03e704730aff848f4d635722d3
mberdev <mathieu.ber.dev@gmail.com>
2024-12-12 21:05:21 +0100

    day12(C#) - part1

--- day12_csharp/ConsoleApp/Program.cs
+++ day12_csharp/ConsoleApp/Program.cs
@@ -1,7 +1,8 @@
 ﻿
+using System.Collections.Immutable;
 using ConsoleApp;
 
-var lines = InputParser.ParseInput("./input_data/test_input2.txt");
+var lines = InputParser.ParseInput("./input_data/input.txt");
 
 // Control
 //foreach (var line in lines)
@@ -11,7 +12,9 @@ var lines = InputParser.ParseInput("./input_data/test_input2.txt");
 
 var grid = new TextBasedGrid(lines);
 
-List<Queue<Position>> regions = new List<Queue<Position>>();
+//List<Queue<Position>> regions = new List<Queue<Position>>();
+
+int price = 0;
 
 for (int y = 0; y < grid.Height; y++)
 {
@@ -24,12 +27,108 @@ for (int y = 0; y < grid.Height; y++)
         }
 
         var regionPlots = grid.floodFill(new Position(x, y), ' ');
-        Console.WriteLine($"Region at {p} has {regionPlots.Count} plots.");
 
-        regions.Add(regionPlots);
+        int regionFences = 0;
+        regionFences += countHorizontalFences(regionPlots);
+        regionFences += countVerticalFences(regionPlots);
+
+        price += regionPlots.Count * regionFences;
+        //Console.WriteLine($"Region at {p} has {regionPlots.Count} plots and {regionFences} fences.");
+
+        //regions.Add(regionPlots);
     }
 }
 
-Console.WriteLine($"There are {regions.Count} regions.");
+//Console.WriteLine($"There are {regions.Count} regions.");
+
+Console.WriteLine($"The price is {price}.");
 
 Console.WriteLine("Done.");
+
+static int countHorizontalFences(Queue<Position> regionPlots)
+{
+    int fencesCount = 0;
+
+    var regionVerticalStripes = regionPlots.GroupBy(p => p.X).ToList();
+    foreach (var verticalStripe in regionVerticalStripes)
+    {
+        if (verticalStripe.Count() == 0)
+        {
+            throw new Exception("This should not happen.");
+        }
+
+        int x = verticalStripe.Key;
+        var plotsSortedTopToBottom = verticalStripe.OrderBy(p => p.Y).ToList();
+
+        bool isOutside = true;
+        int startY = plotsSortedTopToBottom.First().Y - 1;
+        int endY = plotsSortedTopToBottom.Last().Y + 1;
+
+        for (int y = startY; y <= endY; y++)
+        {
+            Position p = new Position(x, y);
+            if (plotsSortedTopToBottom.Contains(p))
+            {
+                if (isOutside)
+                {
+                    isOutside = false;
+                    fencesCount++;
+                    //Console.WriteLine($"Found horizontal fence at {p}.");
+
+                }
+            }
+            else
+            {
+                if (!isOutside)
+                {
+                    isOutside = true;
+                    fencesCount++;
+                    //Console.WriteLine($"Found horizontal fence at {p}.");
+                }
+            }
+        }
+    }
+    return fencesCount;
+
+}
+
+static int countVerticalFences(Queue<Position> regionPlots)
+{
+    int fencesCount = 0;
+    var regionHorizontalStripes = regionPlots.GroupBy(p => p.Y).ToList();
+    foreach (var horizontalStripe in regionHorizontalStripes)
+    {
+        if (horizontalStripe.Count() == 0)
+        {
+            throw new Exception("This should not happen.");
+        }
+        int y = horizontalStripe.Key;
+        var plotsSortedLeftToRight = horizontalStripe.OrderBy(p => p.X).ToList();
+        bool isOutside = true;
+        int startX = plotsSortedLeftToRight.First().X - 1;
+        int endX = plotsSortedLeftToRight.Last().X + 1;
+        for (int x = startX; x <= endX; x++)
+        {
+            Position p = new Position(x, y);
+            if (plotsSortedLeftToRight.Contains(p))
+            {
+                if (isOutside)
+                {
+                    isOutside = false;
+                    fencesCount++;
+                    //Console.WriteLine($"Found vertical fence at {p}.");
+                }
+            }
+            else
+            {
+                if (!isOutside)
+                {
+                    isOutside = true;
+                    fencesCount++;
+                    //Console.WriteLine($"Found vertical fence at {p}.");
+                }
+            }
+        }
+    }
+    return fencesCount;
+}
