e72fa1cc93680f13b09b602ef0180ac8abe9a8c5
mberdev <mathieu.ber.dev@gmail.com>
2024-12-14 01:44:55 +0100

    day09(C#) - deleted useless files from previous days

--- day09_csharp/ConsoleApp/Program.cs
+++ day09_csharp/ConsoleApp/Program.cs
@@ -14,7 +14,10 @@ else
     throw new FileNotFoundException("The specified file was not found.", filePath);
 }
 
-var data = InputParser.ParseInput(lines);
+
+char[] cdata = lines[0].ToCharArray();
+int[] data = Enumerable.Range(0, cdata.Length).ToArray().Select(i => (int)cdata[i]).ToArray();
+
 
 // Control
 //Console.WriteLine(data);
@@ -25,9 +28,75 @@ part1(data);
 
 Console.WriteLine("Done.");
 
-static void part1(Data data)
+static void part1(int[] data)
 {
+    int LENGTH = data.Length;
 
-    Console.WriteLine("TOTAL TOKENS: " + totalTokensSpent);
+    //algorithm needs odd number of compressed blocks
+    if (LENGTH %2 != 1)
+    {
+        throw new Exception("Invalid input length");
+    }
+
+    long checksum = 0;
+    int sourcePos = 0;
+    int targetPos = 0;
+    int dataLength = LENGTH;
+    Queue<int> buffer = new();
+
+    while (sourcePos < dataLength)
+    {
+        // non-gap
+        if (sourcePos % 2 == 0)
+        {
+            int fileID = (sourcePos / 2) % 10;
+            int size = data[sourcePos];
+            int targetEnd = targetPos + size;
+            while (targetPos < targetEnd)
+            {
+                checksum += targetPos * fileID;
+                targetPos++;
+            }
+
+        }
+        //gap to fill
+        else
+        {
+            int gapSize = data[sourcePos];
+            int targetEnd = targetPos + gapSize;
+            while (targetPos < targetEnd)
+            {
+                if (buffer.Count == 0)
+                {
+                    //look at end of file
+                    int lastFilePos = dataLength - 1;
+                    //if file currently ends on a gap, remove it
+                    if (lastFilePos % 2 == 1)
+                    {
+                        lastFilePos--;
+                        dataLength--;
+                    }
+                    // unpack non-gap data into queue
+                    int endFileID = (lastFilePos / 2) % 10;
+                    int dataSize = data[lastFilePos];
+
+                    for (int i = 0; i < dataSize; i++)
+                    {
+                        buffer.Enqueue(endFileID);
+                    }
+                    lastFilePos--;
+                    dataLength--;
+                }
+
+                //take a value, put it into the gap
+                int movedFileID = buffer.Dequeue();
+
+                checksum += targetPos * movedFileID;
+                targetPos++;
+            }
+        }
+
+        sourcePos++;
+    }
 }
 
