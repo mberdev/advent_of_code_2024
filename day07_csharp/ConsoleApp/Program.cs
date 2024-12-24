55d43920b326b0b9d0ec23e438b2a4d4ea211f84
mberdev <mathieu.ber.dev@gmail.com>
2024-12-25 00:53:40 +0100

    day07(c#) - part1 - modified to prepare part2

--- day07_csharp/ConsoleApp/Program.cs
+++ day07_csharp/ConsoleApp/Program.cs
@@ -20,7 +20,8 @@ var data = InputParser.ParseInput(lines);
 // Control
 //Console.WriteLine(data);
 
-part1(data);
+//part1(data);
+part2(data);
 
 void part1(List<(long, List<long>)> data)
 {
@@ -32,7 +33,7 @@ void part1(List<(long, List<long>)> data)
 
         //Control
         //System.Console.WriteLine($"{testValue} : {string.Join(", ", equationÖperands)}");
-        var operators = Solve(testValue, equationOperands.ToArray());
+        var operators = Solve(testValue, equationOperands);
 
         if (operators != null)
         {
@@ -48,19 +49,45 @@ void part1(List<(long, List<long>)> data)
 
 }
 
-Stack<char>? Solve(long testValue, long[] operands)
+void part2(List<(long, List<long>)> data)
 {
-    return TryStep(testValue, operands, operands.Length -1, null);
+    long calibration = 0;
+    foreach (var item in data)
+    {
+        var testValue = item.Item1;
+        var equationOperands = item.Item2;
+
+        //Control
+        //System.Console.WriteLine($"{testValue} : {string.Join(", ", equationÖperands)}");
+        var operators = Solve(testValue, equationOperands);
+
+        if (operators != null)
+        {
+            System.Console.WriteLine($"{testValue} can be solved with {string.Join(",", operators)}");
+            calibration += testValue;
+        }
+        else
+        {
+            System.Console.WriteLine($"   X {testValue}");
+        }
+    }
+    Console.WriteLine($"Total: {calibration}");
 
 }
 
-Stack<char>? TryStep(long value, long[] operands, int operandPosition, char? oprator)
+Stack<char>? Solve(long testValue, List<long> operands)
+{
+    return TryStep(testValue, operands, null);
+
+}
+
+Stack<char>? TryStep(long value, List<long> operands, char? oprator)
 {
     Stack<char>? operators;
 
-    var operand = operands[operandPosition];
+    var operand = operands.Last();
 
-    if (operandPosition == 0)
+    if (operands.Count == 1)
     {
         if (oprator == '*' && operand == value)
         {
@@ -78,9 +105,12 @@ Stack<char>? TryStep(long value, long[] operands, int operandPosition, char? opr
         return null;
     }
 
+    var operandsClone = new List<long>(operands);
+    operandsClone.RemoveAt(operandsClone.Count - 1); // We don't use Stack because cloning a stack reverts it (!) and I have no time for this kind of bs.
+
     if (value % operand == 0)
     {
-        operators = TryStep(value / operand, operands, operandPosition-1, '*');
+        operators = TryStep(value / operand, operandsClone, '*');
         if (operators != null)
         {
             if (oprator != null)
@@ -91,7 +121,7 @@ Stack<char>? TryStep(long value, long[] operands, int operandPosition, char? opr
         }
     }
 
-    operators = TryStep(value - operand, operands, operandPosition-1, '+');
+    operators = TryStep(value - operand, operandsClone, '+');
     if (operators != null)
     {
         if (oprator != null)
