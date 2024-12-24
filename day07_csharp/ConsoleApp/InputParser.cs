029dc0fcf4817e0f8d3a1116d3783052c9169861
mberdev <mathieu.ber.dev@gmail.com>
2024-12-25 00:45:49 +0100

    day07(c#) - part1

--- day07_csharp/ConsoleApp/InputParser.cs
+++ day07_csharp/ConsoleApp/InputParser.cs
@@ -9,9 +9,27 @@ namespace ConsoleApp
     public class InputParser
     {
 
-        //public static List<Item> ParseInput(int[] data)
-        //{
+        public static List<(long, List<long>)> ParseInput(string[] data)
+        {
+            var result = new List<(long, List<long>)>();
+            foreach (var line in data)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    continue;
+                }
 
-        //}
+                var parts = line.Split(':');
+                if (parts.Length != 2)
+                {
+                    throw new Exception($"Invalid input: {line}");
+                }
+                long beforeColon = long.Parse(parts[0]);
+                List<long> afterColon = parts[1].Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(long.Parse).ToList();
+                result.Add((beforeColon, afterColon));
+            
+            }
+            return result;
+        }
     }
 }
--- day07_csharp/ConsoleApp/Program.cs
+++ day07_csharp/ConsoleApp/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using ConsoleApp;
 
 
-var filePath = "./input_data/test_input1.txt";
+var filePath = "./input_data/input.txt";
 string[] lines;
 if (System.IO.File.Exists(filePath))
 {
@@ -14,25 +14,97 @@ else
     throw new FileNotFoundException("The specified file was not found.", filePath);
 }
 
-
+var data = InputParser.ParseInput(lines);
 
 
 // Control
 //Console.WriteLine(data);
 
-//var total = part1(grid, new PositionState(start, Direction.Up));
-//Console.WriteLine($"Part 1: {total}");
+part1(data);
 
-//var total = part2(grid, new PositionState(start, Direction.Up));
-//Console.WriteLine($"Part 2: {total}");
+void part1(List<(long, List<long>)> data)
+{
+    long calibration = 0;
+    foreach (var item in data)
+    {
+        var testValue = item.Item1;
+        var equationOperands = item.Item2;
 
-//Console.WriteLine("Done.");
+        //Control
+        //System.Console.WriteLine($"{testValue} : {string.Join(", ", equationÖperands)}");
+        var operators = Solve(testValue, equationOperands.ToArray());
+
+        if (operators != null)
+        {
+            System.Console.WriteLine($"{testValue} can be solved with {string.Join(",", operators)}");
+            calibration += testValue;
+        }
+        else
+        {
+            System.Console.WriteLine($"   X {testValue}");
+        }
+    }
+    Console.WriteLine($"Total: {calibration}");
+
+}
+
+Stack<char>? Solve(long testValue, long[] operands)
+{
+    return TryStep(testValue, operands, operands.Length -1, null);
+
+}
+
+Stack<char>? TryStep(long value, long[] operands, int operandPosition, char? oprator)
+{
+    Stack<char>? operators;
+
+    var operand = operands[operandPosition];
+
+    if (operandPosition == 0)
+    {
+        if (oprator == '*' && operand == value)
+        {
+            operators = new Stack<char>();
+            operators.Push((char)oprator!);
+            return operators;
+        }
+        else if (oprator == '+' && operand == value)
+        {
+            operators = new Stack<char>();
+            operators.Push((char)oprator!);
+            return operators;
+        }
+        
+        return null;
+    }
+
+    if (value % operand == 0)
+    {
+        operators = TryStep(value / operand, operands, operandPosition-1, '*');
+        if (operators != null)
+        {
+            if (oprator != null)
+            {
+                operators.Push((char)oprator!);
+            }
+            return operators;
+        }
+    }
+
+    operators = TryStep(value - operand, operands, operandPosition-1, '+');
+    if (operators != null)
+    {
+        if (oprator != null)
+        {
+            operators.Push((char)oprator!);
+        }
+        return operators;
+    }
+
+    return null;
+}
 
 
-//static int part1(TextBasedGrid grid, PositionState start)
-//{
-
-//}
 
 //static int part2(TextBasedGrid grid, PositionState start)
 //{
