f746150ba0616680ce98e93e36620a3a5e918bea
mberdev <mathieu.ber.dev@gmail.com>
2024-12-25 14:16:14 +0100

    day08(c#) - boilerplate + data

---
+++ day08_csharp/ConsoleApp/InputParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class InputParser
+    {
+
+        public static Dictionary<char, List<Position>> ParseInput(TextBasedGrid grid)
+        {
+            Dictionary<char, List<Position>> d = new();
+
+            for (int y = 0; y < grid.Height; y++)
+            {
+                for (int x = 0; x < grid.Width; x++)
+                {
+                    var c = grid.GetAt(x, y);
+                    if (c != '.')
+                    {
+                        if (!d.ContainsKey(c))
+                        {
+                            d[c] = new List<Position>();
+                        }
+                        d[c].Add(new Position(x, y));
+                    }
+                }
+            }
+
+            return d;
+        }
+    }
+}
---
+++ day08_csharp/ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using ConsoleApp;
+
+
+var filePath = "./input_data/test_input1.txt";
+string[] lines;
+if (System.IO.File.Exists(filePath))
+{
+    lines = System.IO.File.ReadAllLines(filePath);
+}
+else
+{
+    throw new FileNotFoundException("The specified file was not found.", filePath);
+}
+
+var grid = new TextBasedGrid(lines);
+var antennas = InputParser.ParseInput(grid);
+
+foreach (var (key, value) in antennas)
+{
+    Console.WriteLine($"Antenna {key}: {string.Join(", ", value)}");
+}
+// Control
+//Console.WriteLine(data);
+
+
+//long knownBestScore = part1(grid, start, end); // Needed for part2
+//part2(grid, start, end, knownBestScore);
+
+Console.WriteLine("Done.");
+
+
+//static long part1(TextBasedGrid grid, Position start, Position end)
+//{
+//}
+
+//static void part2(TextBasedGrid textGrid, Position start, Position end, long knownBestScore)
+//{
+//}
