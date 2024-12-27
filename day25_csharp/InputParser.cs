7e4db8ea159653b02d2ed9e19f0afc17dc68e91f
mberdev <mathieu.ber.dev@gmail.com>
2024-12-27 15:35:56 +0100

    day25(c#) - boilerplate + data

---
+++ day25_csharp/InputParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class InputParser
+    {
+
+        //public static (TextBasedGrid, List<Direction>) ParseInput(string[] lines)
+        //{
+        //}
+    }
+}
---
+++ day25_csharp/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using ConsoleApp;
+
+
+
+var filePath = "./input_data/input.txt";
+string[] lines;
+if (System.IO.File.Exists(filePath))
+{
+    lines = System.IO.File.ReadAllLines(filePath);
+}
+else
+{
+    throw new FileNotFoundException("The specified file was not found.", filePath);
+}
+
+
+// Control
+//Console.WriteLine(data);
+
+(var grid, var moves) = InputParser.ParseInput(lines);
+
+
+
+
+//part1(grid, robot, moves);
+
+void part1()
+{
+
+}
+
+Console.WriteLine("Done.");
+
+
+
+
