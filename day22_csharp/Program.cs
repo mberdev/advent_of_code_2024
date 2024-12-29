6ee727cfe1676e990e0871f973e17a7d891188b0
mberdev <mathieu.ber.dev@gmail.com>
2024-12-29 20:21:16 +0100

    day22(c#) - part1

--- day22_csharp/Program.cs
+++ day22_csharp/Program.cs
@@ -4,7 +4,7 @@ using ConsoleApp;
 
 
 
-var filePath = "./input_data/test_input1.txt";
+var filePath = "./input_data/input.txt";
 string[] lines;
 if (System.IO.File.Exists(filePath))
 {
@@ -25,7 +25,23 @@ part1(numbers);
 
 void part1(List<long> numbers)
 {
-    throw new NotImplementedException();
+    var steps = 2000;
+    long total = 0;
+    foreach (var number in numbers)
+    {
+        Console.WriteLine(number + ":");
+        var secretNumber = number;
+        for (var i = 0; i < steps; i++)
+        {
+            secretNumber = SecretNumber.Next(secretNumber);
+            //Console.WriteLine("   (" + (i + 1) + ") " + secretNumber);
+        }
+        Console.WriteLine("   -> " + secretNumber);
+        total += secretNumber;
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Total: " + total);
 }
 
 Console.WriteLine("Done.");
---
+++ day22_csharp/SecretNumber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class SecretNumber
+    {
+        public static long Next(long n)
+        {
+
+            //Calculate the result of multiplying the secret number by 64.Then,
+            //mix this result into the secret number.Finally, prune the secret number.
+
+            //Calculate the result of dividing the secret number by 32.Round
+            //the result down to the nearest integer.Then, mix this result into the
+            //secret number. Finally, prune the secret number.
+
+            //Calculate the result of multiplying the secret number by 2048.Then,
+            //mix this result into the secret number.Finally, prune the secret number.
+            n = mix(n, n * 64);
+            n = prune(n);
+
+            n = mix(n, n / 32);
+            n = prune(n);
+
+            n = mix(n, n * 2048);
+            n = prune(n);
+
+            return n;
+        }
+
+        private static long mix(long n, long v)
+        {
+            return n ^ v;
+        }
+
+        private static long prune(long n)
+        {
+            return n % 16777216;
+        }
+
+    }
+}
