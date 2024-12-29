c3fc34887710b8d0461267ae28da8eeb59f6ab15
mberdev <mathieu.ber.dev@gmail.com>
2024-12-29 20:04:10 +0100

    day22(c#) - part1 - data + boilterplate

---
+++ day22_csharp/InputParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class InputParser
+    {
+
+        public static List<long> ParseInput(string[] lines)
+        {
+            var data = new List<long>();
+            foreach (var line in lines)
+            {
+                if(string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+                data.Add(long.Parse(line));
+            }
+            return data;
+        }
+    }
+}
---
+++ day22_csharp/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using ConsoleApp;
+
+
+
+var filePath = "./input_data/test_input1.txt";
+string[] lines;
+if (System.IO.File.Exists(filePath))
+{
+    lines = System.IO.File.ReadAllLines(filePath);
+}
+else
+{
+    throw new FileNotFoundException("The specified file was not found.", filePath);
+}
+
+
+// Control
+//Console.WriteLine(data);
+
+var numbers = InputParser.ParseInput(lines);
+
+part1(numbers);
+
+void part1(List<long> numbers)
+{
+    throw new NotImplementedException();
+}
+
+Console.WriteLine("Done.");
+
+
+
+
